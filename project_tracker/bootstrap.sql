91a5b27fbc656f94c42695146676e856b34174d5
Alex Tiley <alextiley@gmail.com>
2014-03-21 16:58:26 +0000

    updating sql starter script

--- project_tracker/bootstrap.sql
+++ project_tracker/bootstrap.sql
@@ -2,12 +2,11 @@ BEGIN;
 
 CREATE DATABASE project_tracker default character set utf8;
 
-CREATE TABLE `dashboard_project` (
+CREATE TABLE `projects_project` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `project_name` varchar(120) NOT NULL UNIQUE,
     `deployment_date` datetime NOT NULL,
     `is_closed` bool NOT NULL
-)
-;
+);
 
 COMMIT;
