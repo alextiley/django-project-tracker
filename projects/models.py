127fad46b9ca97fd0b84b80bed81cb27457d3c59
Alex Tiley <alextiley@gmail.com>
2014-03-26 12:04:29 +0000

    added test for default time logic. also added default date functionality

--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+
 from datetime import datetime
 
 class Project(models.Model):
 
+	def default_date():
+		date_now = datetime.now().date()
+		month_now = date_now.month
+		next_month = month_now + 1 if month_now < 12 else 1
+		return date_now.replace(month=next_month)
+
 	def default_time():
-		return datetime.now().replace(hour=12, minute=00, second=0, microsecond=0)
+		return datetime.now().time().replace(hour=12, minute=00, second=0, microsecond=0)
 
 	def __unicode__(self):
 		return str(self.id) + ": " + self.name
 
 	name = models.CharField(max_length = 120, unique = True)
-	deployment_date = models.DateField()
+	deployment_date = models.DateField(default = default_date)
 	deployment_time = models.TimeField(default = default_time)
 	is_complete = models.BooleanField(default = False)
--- projects/tests.py
+++ projects/tests.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
-
+from datetime import datetime
 from projects.models import Project
 
-class ProjectViewTests(TestCase):
+class ProjectModelTests(TestCase):
 
-	def 
+	"""
+	Tests that the default deployment_date of a new project is one month from today
+	"""
+	def test_default_date(self):
+		project = Project()
+		month_now = datetime.now().date().month
+		next_month = month_now + 1 if month_now < 12 else 1
+		self.assertEqual(project.deployment_date.month, next_month)
+
+	"""
+	Tests that the default deployment_time of a new project is 12pm
+	"""
+	def test_default_time(self):
+		project = Project()
+		self.assertEqual(project.deployment_time.hour, 12)
