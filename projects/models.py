9b665edb88e0b9828ba0974bc560dd7f638d3c13
Alex Tiley <alextiley@gmail.com>
2014-03-26 13:49:42 +0000

    changed default date/time logic

--- projects/models.py
+++ projects/models.py
@@ -4,12 +4,14 @@ from datetime import datetime
 
 class Project(models.Model):
 
+	# Set default project date to the 1st of the next calendar month
 	def default_date():
 		date_now = datetime.now().date()
 		month_now = date_now.month
 		next_month = month_now + 1 if month_now < 12 else 1
-		return date_now.replace(month=next_month)
+		return date_now.replace(day=1, month=next_month)
 
+	# Default time is set to 12pm
 	def default_time():
 		return datetime.now().time().replace(hour=12, minute=00, second=0, microsecond=0)
 
--- projects/tests.py
+++ projects/tests.py
@@ -5,12 +5,15 @@ from projects.models import Project
 class ProjectModelTests(TestCase):
 
 	"""
-	Tests that the default deployment_date of a new project is one month from today
+	Tests that the default deployment_date of a new project is the 1st of the next calendar month
 	"""
 	def test_default_date(self):
+
 		project = Project()
 		month_now = datetime.now().date().month
 		next_month = month_now + 1 if month_now < 12 else 1
+		
+		self.assertEqual(project.deployment_date.day, 1)
 		self.assertEqual(project.deployment_date.month, next_month)
 
 	"""
